2129d2016ab10933d18ea30389cd2bc38904867c
kyaw soe thant <kyawsoethant.vespri@gmail.com>
2024-09-21 02:13:47 +0630

    [Enhancement] added blog and question component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,26 @@
 import { useEffect, useState } from "react";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
+import Pagination from "../../components/Pagination/Pagination";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
+  const location = useLocation();
+  const searchQuery = new URLSearchParams(location.search);
+  const page = searchQuery.get("page");
 
   useEffect(() => {
     const fetchRecipes = async () => {
-      const responseData = await fetch("http://localhost:8000/api/recipes");
+      const responseData = await fetch(
+        "http://localhost:8000/api/recipes?page=" + page
+      );
       if (responseData.ok) {
         const data = await responseData.json();
         setRecipes(data);
       }
     };
     fetchRecipes();
-  }, []);
+  }, [page]);
 
   return (
     <div className="space-y-3">
@@ -21,6 +28,7 @@ const Home = () => {
         recipes.map((recipe) => (
           <RecipeCard recipe={recipe} key={recipe._id} />
         ))}
+      <Pagination />
     </div>
   );
 };
